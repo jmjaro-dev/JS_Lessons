c7a2e77697374e537bc5eef47d81836db6197161
jmjaro-dev <jmjaropojop92@gmail.com>
2019-09-03 16:25:36 +0800

    Sept 3 - OOP - Built In Constructors

---
+++ 4_OOP/2_builtIn_constructors.js
@@ -0,0 +1,47 @@
+// BUILT IN CONSTRUCTOR
+// // String Constructor
+
+// const name1 = 'Jeff';
+// const name2 = new String('Jeff');
+
+// // console.log(typeof name2);
+
+// if(name2 === 'Jeff') {
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+// // Number Constructor
+// const num1 = 5;
+// const num2 = new Number(5);
+
+// console.log(typeof num2);
+
+// // Boolean Constructor
+// const bool1 = true;
+// const bool2 = new Boolean(true);
+
+// console.log(typeof bool2);
+
+// // Function Constructor
+
+// const getSum1 = function(x, y) {
+//   return x + y;
+// };
+
+// const getSum2 = new Function('x', 'y', 'return x + y');
+
+// console.log(getSum2(2, 3));
+
+// // Object Constructor
+// const john1 = {name: 'John'};
+// const john2 = new Object({name: 'John'});
+
+// console.log(typeof john2);
+
+// // Array Constructor
+// const arr1 = [1,2,3,4];
+// const arr2 = new Array(1,2,3,4);
+
+// console.log( arr2);
