8734f1b646d52c6d077977048e7f75b54f0997b4
jmjaro-dev <jmjaropojop92@gmail.com>
2019-09-09 11:59:12 +0800

    Sept 9 [Promises] - Resolved and Reject

--- 9_ES6-Promises/app.js
+++ 9_ES6-Promises/app.js
@@ -1,2 +1,45 @@
 // ES6 Promises
 
+const posts = [
+  {
+    title: 'Post One', 
+    body: 'This is post One'
+  },
+  {
+    title: 'Post Two', 
+    body: 'This is post Two'
+  }
+];
+
+function createPost(post) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      posts.push(post);
+
+      const error = false;
+      
+      if(!error) {
+        resolve();
+      } else {
+        reject('Error: Something went wrong');
+      }
+    }, 2000);
+  });
+}
+  
+function getPosts() {
+  setTimeout(function() {
+    let output = '<ul class="collection">';
+    
+    posts.forEach(function(post) {
+      output += `<li class="collection-item">${post.title}</li> <li class="collection-item">${post.body}</li>`;
+    });
+    document.querySelector('#posts').innerHTML = `${output} </ul>`;
+  },1000);
+}
+
+createPost({title: 'Post Three', body: 'This is post Three'})
+  .then(getPosts)
+  .catch(function(err) {
+    console.log(err);
+  });
